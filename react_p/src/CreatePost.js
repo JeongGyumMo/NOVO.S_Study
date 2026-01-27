ec1fb01286064e227b72dc7eac54a57e983c163b
chea <ypd06220@naver.com>
2026-01-27 14:35:39 +0900

    2. 글작성/수정/삭제 구현

--- react_p/src/CreatePost.js
+++ react_p/src/CreatePost.js
@@ -1,20 +1,46 @@
-import React from 'react';
 import './css/CreatePost.css';
+import {useState} from "react";
+import axios from "axios";
 
-function CreatePost() {
+let CreatePost = () => {
+
+    let [formData, setFormData] = useState({
+        title: '',
+        content: ''
+    });
+
+    let onChangeFormData = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name] : e.target.value
+        });
+    };
+
+    const onClickSubmit = () => {
+    console.log("API URL:", process.env.REACT_APP_API_URL);
+
+        axios.post(`${process.env.REACT_APP_API_URL}/post`, formData)
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
+
     return (
-        <div className="create-post-container">
-            <h1 className="create-post-title">게시글 작성</h1>
-            <form className="create-post-form">
-                <div className="form-group">
-                    <label htmlFor="title">제목</label>
-                    <input id="title" type="text" placeholder="제목을 입력하세요" />
+        <div className={"create-post-container"}>
+            <h1 className={"create-post-title"}>게시글 작성</h1>
+            <form className={"create-post-form"}>
+                <div className={"form-group"}>
+                    <label htmlFor={"title"}>제목</label>
+                    <input id={"title"} type={"text"} name={"title"} onChange={onChangeFormData} placeholder={"제목을 입력하세요"}/>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="content">내용</label>
-                    <textarea id="content" placeholder="내용을 입력하세요" />
+                <div className={"form-group"}>
+                    <label htmlFor={"content"}>내용</label>
+                    <textarea id={"content"} name={"content"} onChange={onChangeFormData} placeholder={"내용을 입력하세요"}/>
                 </div>
-                <button type="submit" className="submit-button">등록</button>
+                <button type={"submit"} className={"submit-button"} onClick={onClickSubmit}>등록</button>
             </form>
         </div>
     );
