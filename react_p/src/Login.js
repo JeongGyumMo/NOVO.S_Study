57ce70d3fe21ca8c2ed78d873dba0950709665fc
chea <ypd06220@naver.com>
2026-02-16 17:25:24 +0900

    10. 로그인 화면 css 변경

--- react_p/src/Login.js
+++ react_p/src/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import api from "./api/axios";
 
+
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -23,13 +24,28 @@ function Login() {
 
 
   return (
-    <div>
+   <div style={{
+     display: "flex",
+     justifyContent: "center",
+     alignItems: "center",
+     height: "80vh"
+   }}>
+     <div style={{
+         width: "320px",
+         padding: "30px",
+         border: "1px solid #ddd",
+         borderRadius: "10px",
+         textAlign: "center",
+         boxShadow: "0 4px 10px rgba(0,0,0,0.1)"
+     }}>
+
       <h2>로그인</h2>
       <input
         type="text"
         placeholder="아이디"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        style={{ width: "85%", padding: "10px", marginBottom: "0 auto 10px", margin: "0 auto 15px"}}
       />
       <br />
       <input
@@ -37,10 +53,25 @@ function Login() {
         placeholder="비밀번호"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        style={{ width: "85%", padding: "10px", marginBottom: "0 auto 10px", margin: "0 auto 15px"}}
       />
       <br />
-      <button onClick={handleLogin}>로그인</button>
+      <button onClick={handleLogin}
+      style={{
+          width: "90%",
+          padding: "10px",
+          backgroundColor: "#4CAF50",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+          fontWeight: "bold"
+        }}
+      >
+        로그인
+      </button>
     </div>
+   </div>
   );
 }
 
